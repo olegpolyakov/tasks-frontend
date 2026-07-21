784e7cd72fae9d5eec76ca16721df29ecec29a3a
Oleg Polyakov <olegpolyakov@outlook.com>
2026-07-22 00:09:07 +0300

    refactor: reorganize utility functions in utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,12 @@
+// TODO Move to a shared lib
+
 export function toRecord<T extends {id: number | string}>(list: T[]): Record<string, T> {
     return list.reduce((record, item) => ({
         ...record,
         [item.id]: item
     }), {});
+}
+
+export function capitalize(string: string) {
+    return string.at(0)?.toUpperCase() + string.slice(1);
 }
