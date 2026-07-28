bc45bca68e30d0f4da03271641ea2f09e8f7b945
Oleg Polyakov <olegpolyakov@outlook.com>
2026-07-28 15:29:47 +0300

    Update children test

--- test/features/tasks/logic/children.test.ts
+++ test/features/tasks/logic/children.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, test } from 'vitest';
 
-import type { TaskData } from '@olegpolyakov/tasks-core';
+import type { Task } from '@olegpolyakov/tasks-core';
 
 import { buildTree, getAllChildren } from '@/features/tasks/logic/children';
 
@@ -10,8 +10,8 @@ describe('getAllChildren', () => {
     describe('given a task without children', () => {
         test('returns an empty array', () => {
             const id = '1';
-            const record: Record<string, TaskData> = {
-                [id]: { id: '1', childrenIds: [] as string[] } as TaskData
+            const record: Record<string, Task> = {
+                [id]: { id: '1', childrenIds: [] as string[] } as Task
             };
 
             expect(getAllChildren(id, record)).toHaveLength(0);
@@ -21,9 +21,9 @@ describe('getAllChildren', () => {
     describe('given a task with 1 child', () => {
         test('returns an array with 1 item', () => {
             const id = '1';
-            const record: Record<string, TaskData> = {
-                1: { id: '1', childrenIds: ['2'] as string[] } as TaskData,
-                2: { id: '2', childrenIds: [] as string[] } as TaskData
+            const record: Record<string, Task> = {
+                1: { id: '1', childrenIds: ['2'] as string[] } as Task,
+                2: { id: '2', childrenIds: [] as string[] } as Task
             };
 
             expect(getAllChildren(id, record)).toHaveLength(1);
@@ -33,10 +33,10 @@ describe('getAllChildren', () => {
     describe('given a task with 2 children', () => {
         test('returns an array with 2 items', () => {
             const id = '1';
-            const record: Record<string, TaskData> = {
-                1: { id: '1', childrenIds: ['2', '3'] as string[] } as TaskData,
-                2: { id: '2', childrenIds: [] as string[] } as TaskData,
-                3: { id: '3', childrenIds: [] as string[] } as TaskData
+            const record: Record<string, Task> = {
+                1: { id: '1', childrenIds: ['2', '3'] as string[] } as Task,
+                2: { id: '2', childrenIds: [] as string[] } as Task,
+                3: { id: '3', childrenIds: [] as string[] } as Task
             };
 
             expect(getAllChildren(id, record)).toHaveLength(2);
@@ -46,10 +46,10 @@ describe('getAllChildren', () => {
     describe('given a task with 1 child with 1 child', () => {
         test('returns an array with 2 items', () => {
             const id = '1';
-            const record: Record<string, TaskData> = {
-                1: { id: '1', childrenIds: ['2'] as string[] } as TaskData,
-                2: { id: '2', childrenIds: ['3'] as string[] } as TaskData,
-                3: { id: '3', childrenIds: [] as string[] } as TaskData
+            const record: Record<string, Task> = {
+                1: { id: '1', childrenIds: ['2'] as string[] } as Task,
+                2: { id: '2', childrenIds: ['3'] as string[] } as Task,
+                3: { id: '3', childrenIds: [] as string[] } as Task
             };
 
             expect(getAllChildren(id, record)).toHaveLength(2);
@@ -59,14 +59,14 @@ describe('getAllChildren', () => {
     describe('given a task 2 children each containing 2 children', () => {
         test('returns an array with 2 items', () => {
             const id = '1';
-            const record: Record<string, TaskData> = {
-                1: { id: '1', childrenIds: ['2', '3'] as string[] } as TaskData,
-                2: { id: '2', childrenIds: ['4', '5'] as string[] } as TaskData,
-                3: { id: '3', childrenIds: ['6', '7'] as string[] } as TaskData,
-                4: { id: '4', childrenIds: [] as string[] } as TaskData,
-                5: { id: '5', childrenIds: [] as string[] } as TaskData,
-                6: { id: '6', childrenIds: [] as string[] } as TaskData,
-                7: { id: '7', childrenIds: [] as string[] } as TaskData
+            const record: Record<string, Task> = {
+                1: { id: '1', childrenIds: ['2', '3'] as string[] } as Task,
+                2: { id: '2', childrenIds: ['4', '5'] as string[] } as Task,
+                3: { id: '3', childrenIds: ['6', '7'] as string[] } as Task,
+                4: { id: '4', childrenIds: [] as string[] } as Task,
+                5: { id: '5', childrenIds: [] as string[] } as Task,
+                6: { id: '6', childrenIds: [] as string[] } as Task,
+                7: { id: '7', childrenIds: [] as string[] } as Task
             };
 
             console.log(getAllChildren(id, record));
