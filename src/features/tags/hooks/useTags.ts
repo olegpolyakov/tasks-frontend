b70159f171d1ccc91b2bb55e3506f49bcb589867
Oleg Polyakov <olegpolyakov@outlook.com>
2026-08-19 13:55:19 +0300

    Add useTagsState hook

--- src/features/tags/hooks/useTags.ts
+++ src/features/tags/hooks/useTags.ts
@@ -1,46 +1,28 @@
-import { useCallback, useEffect } from 'react';
-
-import { useAtom } from 'jotai';
+import { useCallback } from 'react';
 
 import { Tag } from '@olegpolyakov/tasks-core';
 
-import { tagsAtom } from '../state';
-
 import useTagsApi from './useTagsApi';
+import useTagsState from './useTagsState';
 
 export default function useTags() {
     const api = useTagsApi();
-
-    const [tags, setTags] = useAtom(tagsAtom);
-
-    useEffect(() => {
-        api.fetchTags().then(setTags);
-    }, [api, setTags]);
+    const tags = useTagsState(api);
 
     const createTag = useCallback(async (data: Partial<Tag>) => {
-        const createdTag = await api.createTag(data);
-
-        setTags(prevTags => [...prevTags, createdTag]);
-
-        return createdTag;
-    }, [api, setTags]);
+        return api.createTag(data);
+    }, [api]);
 
     const updateTag = useCallback(async (id: string, data: Partial<Tag>) => {
-        const updatedTag = await api.updateTag(id, data);
-
-        setTags(tags => tags.map(tag => tag.id === updatedTag.id ? updatedTag : tag));
-
-        return updatedTag;
-    }, [api, setTags]);
+        return api.updateTag(id, data);
+    }, [api]);
 
     const deleteTag = useCallback(async (id: string) => {
         await api.deleteTag(id);
-        
-        setTags(tags => tags.filter(tag => tag.id !== id));
-    }, [api, setTags]);
+    }, [api]);
 
     return {
-        tags: tags.map(data => new Tag(data)),
+        tags,
         createTag,
         updateTag,
         deleteTag
---
+++ src/features/tags/hooks/useTagsState.ts
@@ -0,0 +1,27 @@
+import { useEffect, useMemo } from 'react';
+
+import { useAtom } from 'jotai';
+
+import { Tag, type TagData } from '@olegpolyakov/tasks-core';
+
+import { useStateEvents } from '@/store';
+
+import type { TagsApi } from '../api';
+import { tagsAtom, tagsReducer } from '../state';
+
+export default function useTagsState(api: TagsApi) {
+    const [state, setState] = useAtom(tagsAtom);
+
+    useEffect(() => {
+        api.fetchTags().then(setState);
+    }, [api, setState]);
+
+    useStateEvents<TagData>(
+        api.events,
+        action => setState(state => tagsReducer(state, action))
+    );
+
+    const tasks = useMemo(() => state.map(s => new Tag(s)), [state]);
+
+    return tasks;
+}
